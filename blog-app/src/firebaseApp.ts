34cde0744f63c33b0a53d91d5813dbf919b2587f
kangsinbeom <kanian16@naver.com>
2024-01-03 16:13:07 +0900

    feat: set router and firebase

---
+++ blog-app/src/firebaseApp.ts
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, FirebaseApp, getApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import "firebase/auth";
+
+export let app: FirebaseApp;
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTHMDOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENTER_ID,
+  appId: import.meta.env.VITE_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+};
+
+try {
+  app = getApp("app");
+} catch (e) {
+  app = initializeApp(firebaseConfig, "app");
+}
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebase);
+
+export default firebase;
